c1f5a63dc6f4a4229f0d63988159738c9f0a477f
Ziwen Chen <ziwenc@Ziwens-MacBook-Pro.local>
2024-11-04 20:40:33 -0800

    clusten qk

--- clusten.py
+++ clusten.py
@@ -15,7 +15,7 @@ clusten_qk_op_lib_path = str(
     next(
         Path(
             next(Path(os.path.join(base_dir, "build")).glob("lib.linux-x86_64-*"))
-        ).glob("clusten_qk.cpython-*-x86_64-linux-gnu.so")
+        ).glob("clusten.cpython-*-x86_64-linux-gnu.so")
     )
 )
 torch.ops.load_library(clusten_qk_op_lib_path)
@@ -31,7 +31,7 @@ class CLUSTENQKFunction(torch.autograd.Function):
         if key.dtype != query.dtype:
             key = key.to(query.dtype)
         nbhd_idx = nbhd_idx.contiguous()
-        attn = torch.ops.custom_op.clusten_qk(
+        attn = torch.ops.custom_op.clusten_qk_fwd(
             query,
             key.permute(0, 1, 3, 2).contiguous(),
             nbhd_idx)
@@ -40,6 +40,6 @@ class CLUSTENQKFunction(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_attn):
-        outputs = torch.ops.custom_op.clusten_qk_backward(grad_attn.contiguous(), *ctx.saved_tensors)
+        outputs = torch.ops.custom_op.clusten_qk_bwd(grad_attn.contiguous(), *ctx.saved_tensors)
         d_query, d_key = outputs
         return d_query, d_key, None
