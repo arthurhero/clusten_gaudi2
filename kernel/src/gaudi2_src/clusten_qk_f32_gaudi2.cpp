8f24b0997ec345624ac643b1dc8001bbe87d4c86
Ziwen Chen <ziwenc@10-248-5-15.wireless.oregonstate.edu>
2024-11-05 00:27:13 -0800

    clusten qk

--- kernel/src/gaudi2_src/clusten_qk_f32_gaudi2.cpp
+++ kernel/src/gaudi2_src/clusten_qk_f32_gaudi2.cpp
@@ -101,7 +101,7 @@ tpc_lib_api::GlueCodeReturn CLUSTENQKF32Gaudi2::GetGcDefinitions(
             in_defs->outputTensors[1].geometry.dataType = tpc_lib_api::DATA_F32;
             return tpc_lib_api::GLUE_INCOMPATIBLE_DATA_TYPE;
         }
-        std::cout << "out tensor" << in_defs->outputTensors[1].pData << std::endl; 
+        std::cout << "out tensor" << in_defs->outputTensors[1].pData[0] << std::endl; 
         in_defs->outputTensors[1].pData = (float*)calloc(in_defs->outputTensors[1].geometry.maxSizes[0] * in_defs->outputTensors[1].geometry.maxSizes[1] * in_defs->outputTensors[1].geometry.maxSizes[2] * in_defs->outputTensors[1].geometry.maxSizes[3], sizeof(float));
     }
     else
